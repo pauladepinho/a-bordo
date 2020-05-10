743bf71639d80b576c65f8dadafb19b3b5feeee5
pauladepinho <pauladepinho.rj@gmail.com>
2020-05-10 00:03:44 -0300

    início do setup das rotas e controllers com 2 abordagens para tratar dos tipos diferentes de usuários + formatação de números de telefone

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const methodOverride = require("method-override");
 
-var indexRouter = require('./routes/index');
+var IndexRouter = require('./routes/IndexRouter');
 // var usersRouter = require('./routes/users');
 
 var app = express();
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride("_method"));
 
-app.use('/', indexRouter);
+app.use('/', IndexRouter);
 // app.use('/usuario', usersRouter);
 
 // catch 404 and forward to error handler
