aac5c210a700b076b344d5556e063a9a5d9e9773
pauladepinho <pauladepinho.rj@gmail.com>
2020-05-14 21:58:47 -0300

    evaluation_user model

---
+++ models/Evaluation_User.js
@@ -0,0 +1,52 @@
+module.exports = (sequelize, DataTypes) => {
+    let Evaluation_User = sequelize.define(
+        "evaluation_user",
+        {
+            evaluated: {
+                type: DataTypes.TINYINT,
+                allowNull: false
+            },
+            grade: {
+                type: DataTypes.DECIMAL(5, 2),
+                allowNull: false
+            },
+            evaluations_id: {
+                type: DataTypes.INTEGER.UNSIGNED,
+                allowNull: false,
+                // references: {
+                //     model: "evaluations",
+                //     key: "id",
+                // },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE"
+            },
+            users_id: {
+                type: DataTypes.INTEGER.UNSIGNED,
+                allowNull: false,
+                // references: {
+                //     model: "users",
+                //     key: "id",
+                // },
+                onUpdate: "CASCADE",
+                onDelete: "CASCADE"
+            }
+        },
+        {
+            tableName: "evaluations_users",
+            timestamps: false,
+        }
+    );
+
+    Evaluation_User.associate = (models) => {
+        Evaluation_User.belongsTo(models.users, {
+            foreignKey: "users_id",
+            as: "users"
+        });
+        Evaluation_User.belongsTo(models.evaluations, {
+            foreignKey: "evaluations_id",
+            as: "evaluations"
+        });
+    };
+
+    return Evaluation_User;
+};
