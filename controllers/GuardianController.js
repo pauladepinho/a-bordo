8f73d137ea67cb5aaab081d08ea28bb27eac7c1e
Patricia Felix <patriciafelixx@outlook.com>
2020-06-12 10:54:16 -0300

    Recebendo os dados do aluno na pag do responsável

--- controllers/GuardianController.js
+++ controllers/GuardianController.js
@@ -2,15 +2,68 @@ const { User, School, Subject, Student, Teacher, Guardian, Class, Course, Studen
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+const getGuardianData = async (user) => {
+
+    const students = [];
+    const classesStudents = [];
+    const classes = [];
+    const courses = [];
+    const teachers = [];
+    const subjects = [];
+    const schools = [];
+
+    const guardian = await Guardian.findOne({ where: { userId: user.id } });
+    const studentsGuardian = await Student_Guardian.findAll({ where: { guardianId: guardian.id } });
+
+    for (studentGuardian of studentsGuardian) {
+        const student = await Student.findOne({ where: { id: studentGuardian.studentId } });
+        students.push(student);
+
+        const classStudent = await Class_Student.findAll({ where: { studentId: studentGuardian.studentId } })
+        classesStudents.push(...classStudent);
+    }
+
+    for (classStudent of classesStudents) {
+        const clas = await Class.findOne({ where: { id: classStudent.classId}})
+        classes.push(clas);
+    }
+
+    for (clas of classes) {
+        const course = await Course.findAll({ where: { classId: clas.id}})
+        courses.push(...course);
+        const school = await School.findOne({ where: {id: clas.schoolId}});        
+        schools.push(school);
+        console.log(school)
+    }
+
+    for (course of courses) {
+        const teacher = await Teacher.findOne({where: {id: course.teacherId}})
+        const subject = await Subject.findOne({where: {id: course.subjectId}})
+        teachers.push(teacher);
+        subjects.push(subject);
+    }
+
+    // DATA
+    return {
+        students,
+        classes,
+        courses,
+        teachers,
+        subjects,
+        schools
+    }
+}
+
 module.exports = {
 
     // GET responsavel/
     // GET responsavel/home
     renderHome: async (req, res) => {
-        // GET GUARDIAN DATA FROM DB,
-        // AND THEN...
         const user = req.session.user;
-        return res.render("guardian", { user });
+
+        let data = await getGuardianData(user);
+
+        return res.render("guardian", { user, data });
     },
 
     // GET responsavel/cadastrar
